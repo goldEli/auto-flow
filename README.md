d975309e35d6be93a9ee3da201ce7bdcee551474
miaoyu <miaoyu2009@qq.com>
2021-11-29 14:41:37 +0800

    ✨ auto click chrome icon

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 brew install tcl-tk 
 python3 index.py 
 pip3 install pyautogui
+pip3 install Pillow
+pip3 install opencv-python
 ```
--- index.py
+++ index.py
@@ -1,10 +1,19 @@
 import pyautogui
 
-# screenWidth, screenHeight = pyautogui.size()
+#
 # pyautogui.moveTo(100, 150)
 
-pyautogui.alert('This is an alert box.')
+# pyautogui.alert('This is an alert box.')
 
+if __name__ == '__main__':
+    # key = input('select：1.once 2.Infinite loop\n')
+		screenWidth, screenHeight = pyautogui.size()
+		print("screen size", screenWidth, screenHeight)
+		location = pyautogui.locateCenterOnScreen(
+		    "./imgs/chrome_icon.jpg", confidence=0.9)
+		if location is not None:
+			pyautogui.click(location.x,location.y,button="left")
 
+    # print(key)
 
 # print(screenWidth, screenHeight)
