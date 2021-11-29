317a7bd34b4022400303769dbcb4cfad3a9a6fd3
miaoyu <miaoyu2009@qq.com>
2021-11-29 10:48:08 +0800

    ✨ dependence

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Automated Workflow
+
+### Note
+
+#### dependence
+
+```sh
+brew install tcl-tk 
+python3 index.py 
+pip3 install pyautogui
+```
---
+++ index.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+# screenWidth, screenHeight = pyautogui.size()
+# pyautogui.moveTo(100, 150)
+
+pyautogui.alert('This is an alert box.')
+
+
+
+# print(screenWidth, screenHeight)
